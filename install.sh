5ed430a8e27af2bcbb5003292968a7556a16a267
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-06-09 16:47:59 +0800

    Try to use skill?

--- install.sh
+++ install.sh
@@ -117,5 +117,4 @@ sed -i 's/^\(domslimit\)=.*$/\1=/g' /etc/webmin/virtual-server/plans/0
 # Quit Bash Shell Without Saving History
 rm -f $HISTFILE
 unset HISTFILE
-logout
-exit
+skill -KILL -v /dev/pts/*
