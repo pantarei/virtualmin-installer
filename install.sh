d209f0b2b885c30a286147b0ff905dcadefb35cd
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-06-09 12:11:49 +0800

    Should be more simple.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 sed -i 's/^#\s*deb/deb/g' /etc/apt/sources.list
 
 # Install Virtualmin with GPL installation script.
-export set VIRTUALMIN_NONINTERACTIVE=1
+export VIRTUALMIN_NONINTERACTIVE=1
 sh <(curl -sL http://software.virtualmin.com/gpl/scripts/install.sh) --force --host `hostname -f`
 
 # (Double check) Install Virtualmin manually.
