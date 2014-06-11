74f03d9e527d9ec4e99623478b8eedddfa6f6c0e
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-06-11 09:44:32 +0800

    Add post_install.sh.

---
+++ post_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o xtrace
+
+# Update resolv.conf with "nameserver 127.0.0.1".
+sed -i 's/^#\(prepend domain-name-servers\).*$/\1 127.0.0.1;/g' /etc/dhcp/dhclient.conf
+/etc/init.d/networking restart
+/etc/init.d/resolvconf restart
+cat /etc/resolv.conf 
+
+# Print the clean history command.
+cat <<-EOF
+rm -f $HISTFILE && unset HISTFILE && exit
+EOF
