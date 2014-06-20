6e25709d21999934033ab04b874cce00d4417fe9
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-06-20 09:06:15 +0800

    Move default VIM profile under phpshift project.

--- install.sh
+++ install.sh
@@ -82,7 +82,7 @@ do
     sed -i 's/^;*\(upload_max_filesize\) =.*$/\1 = 32M/g' $line
 done
 
-bash <(curl -sL https://github.com/pantarei/vim-pathogen/raw/master/install.sh)
+bash <(curl -sL https://github.com/phpshift/vim-pathogen/raw/master/install.sh)
 
 # Additional webmin/virtualmin configuration.
 cat >> /etc/webmin/virtual-server/config <<-EOF
